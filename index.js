ee7e3a81b3449a0e8d52e00f9151865f2dfd2cfd
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-03 11:55:23 -0300

    chore: Cadastro de usuários funcionando

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
-
-
-//Configuração especial
-
-
 const express = require('express')
-const { default: mongoose } = require('mongoose')
+const mongoose = require('mongoose')
 const app = express()
+
+const Person = require('./models/Person')
+
+// Middleware para analisar o corpo da requisição
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Rotas da API
+app.post('/person', async (req, res) => {
+    // Extrair dados do corpo da requisição
+    const { name, salary, approved } = req.body
+    const person = { name, salary, approved }
+    
+    // Criar novo documento no MongoDB
+    try {
+        await Person.create(person)
+        res.status(201).json({ message: 'Pessoa inserida no sistema com sucesso' })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+// Rota inicial / endpoint
+app.get('/', (req, res) => {
+    res.json({ message: 'Olá mundo' })
+})
+
+// Conexão ao MongoDB
 const db_user = 'Kaua'
 const db_password = encodeURIComponent('Kaua!!24092007')
 
-//Ler json / middleware
-app.use(
-    express.urlencoded({
-        extendend: true,
-    }),
-)
-app.use(express.json())
-
-//Rota inicial /endpoint
-app.get('/', (req, res) => {
-
-    //Mostrar req
-
-    res.json({ message: 'Olá mundo'})
-
-})
-
-//Entregar uma porta 
 mongoose
-.connect(
-    `mongodb+srv://${db_user}:${db_password}@cluster0.3pfy0z8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-)
-.then(() => {
-    console.log('Conectamos ao MongoDB')
-    app.listen(3000)
-})
-.catch((err) => console.log(err))
+    .connect(`mongodb+srv://${db_user}:${db_password}@cluster0.3pfy0z8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Conectamos ao MongoDB')
+        app.listen(3000, () => {
+            console.log('Servidor rodando na porta 3000')
+        })
+    })
+    .catch((err) => console.log(err))
+
+
+
