f10ece985ccf6a30d391fcc5e6ab9128c3448a92
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-03 11:39:29 -0300

    CHORE: conexão com o mongodb

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+
+
 //Configuração especial
 
+
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
+const db_user = 'Kaua'
+const db_password = encodeURIComponent('Kaua!!24092007')
 
 //Ler json / middleware
 app.use(
@@ -21,4 +27,12 @@ app.get('/', (req, res) => {
 })
 
 //Entregar uma porta 
-app.listen(3000)
+mongoose
+.connect(
+    `mongodb+srv://${db_user}:${db_password}@cluster0.3pfy0z8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+)
+.then(() => {
+    console.log('Conectamos ao MongoDB')
+    app.listen(3000)
+})
+.catch((err) => console.log(err))
---
+++ readme.md
@@ -0,0 +1,2 @@
+mongodb+srv://Kaua:<Kaua!!24092007>@cluster0.3pfy0z8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+mongodb+srv://Kaua:<Kauals24092007>@cluster0.3pfy0z8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
