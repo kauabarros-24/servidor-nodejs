2100a7785a4da283e08b8806e13539947dc7957b
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-05 12:29:28 -0300

    Método delete implementado

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -42,6 +42,7 @@ router.get('/:id', async (req, res) => {
 
         if (!person) {
             res.status(422).json({message: "O usuário não foi encontrado"})
+            return
         }
 
         res.status(201).json(person)
@@ -64,11 +65,36 @@ router.patch("/:id",  async (req, res) => {
 
     try {
         const updatePerson = await Person.updateOne({_id: id}, person)
+
+        if (updatePerson.matchedCount === 0) {
+            res.status(422).json({message: "Usuário não encontrado"})
+            return
+        }
         
         res.status(200).json(person)
     } catch (error) {
         res.status(500).json({error: error})
     }
+})
+
+//Deleção de dados
+router.delete('/:id', async (req, res) => {
+    const id  = req.params.id;
+
+    const person = await Person.findOne({_id: id})
+
+    if (!person) {
+        res.status(422).json({message: "Usuário não encontrado"})
+        return
+    }
+    try {
+        await Person.deleteOne({_id: id})
+
+        res.status(200).json({message: "Usuário removido com sucesso"})
+    } catch(error) {
+        res.status(500).json({error: error})
+    }
+
 })
 
 module.exports = router
